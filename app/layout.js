720ab484c8d40ee359dfb5e594f488696b77ddea
Mark Chavez <markchavez165@gmail.com>
2023-06-12 21:34:59 -0400

    small changes in bg

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,6 @@ export const metadata = {
 
 {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"/> */}
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
